6156b6f0bef792dbc5263c28f6497148e3533a18
Milan Nosal <milan.nosal@gmail.com>
2023-05-31 22:23:35 +0200

    SourceFile criterion

---
+++ src/main/java/com/github/sulir/jamabuild/criteria/SourceFile.java
@@ -0,0 +1,54 @@
+package com.github.sulir.jamabuild.criteria;
+
+import com.github.sulir.jamabuild.Project;
+import com.github.sulir.jamabuild.filtering.AllowedPhases;
+import com.github.sulir.jamabuild.filtering.Criterion;
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.stream.Stream;
+
+@AllowedPhases(Criterion.Phase.PRE_BUILD)
+public class SourceFile extends Criterion {
+    private final String globPattern;
+    private final String contentToSearch;
+
+    public SourceFile(Phase phase, Type type, String globPattern) {
+        super(phase, type);
+        this.globPattern = globPattern;
+        this.contentToSearch = null;
+    }
+
+    public SourceFile(Phase phase, Type type, String globPattern, String contentToSearch) {
+        super(phase, type);
+        this.globPattern = globPattern;
+        this.contentToSearch = contentToSearch;
+    }
+
+    @Override
+    public boolean isMet(Project project) {
+        try (Stream<Path> paths = Files.walk(project.getSourceDir())) {
+            return paths.anyMatch(path -> matchesGlobAndContent(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean matchesGlobAndContent(Path path) {
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + globPattern);
+        if (matcher.matches(path)) {
+            if (contentToSearch != null) {
+                try {
+                    return FileUtils.readFileToString(path.toFile(), "UTF-8").contains(contentToSearch);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/com/github/sulir/jamabuild/filtering/CriterionFactory.java
+++ src/main/java/com/github/sulir/jamabuild/filtering/CriterionFactory.java
@@ -2,6 +2,7 @@ package com.github.sulir.jamabuild.filtering;
 
 import com.github.sulir.jamabuild.criteria.AndroidSource;
 import com.github.sulir.jamabuild.criteria.BashScript;
+import com.github.sulir.jamabuild.criteria.SourceFile;
 
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
@@ -10,7 +11,8 @@ import java.util.List;
 public class CriterionFactory {
     private static final List<Class<? extends Criterion>> criteria = List.of(
             AndroidSource.class,
-            BashScript.class);
+            BashScript.class,
+            SourceFile.class);
     private final Criterion.Phase phase;
     private final Criterion.Type type;
 
